58f3bbd481330dc43e01458863393334fbe2f94f
shubham.nagure <shubham.nagure@abzooba.com>
2024-07-12 17:40:39 +0530

    added jwt authentication and permission to views

--- spotify/music/views.py
+++ spotify/music/views.py
@@ -1,31 +1,36 @@
-from django.shortcuts import render
 from .models import Artist,Album, Track
 from .serializers import ArtistsSerializer,AlbumSerializer, TrackSerializer
 from rest_framework import generics
-
+from rest_framework.permissions import IsAuthenticated
 
 class tracksView(generics.ListCreateAPIView):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
+    permission_classes = [IsAuthenticated]
 
 class trackDetailView(generics.RetrieveAPIView):
     queryset = Track.objects.all()
     serializer_class = TrackSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class ArtistsView(generics.ListCreateAPIView):
     queryset = Artist.objects.all()
     serializer_class = ArtistsSerializer
+    permission_classes = [IsAuthenticated]
 
 class ArtistDetailView(generics.RetrieveAPIView):
     queryset = Artist.objects.all()
     serializer_class = ArtistsSerializer
+    permission_classes = [IsAuthenticated]
 
 class AlbumsView(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    permission_classes = [IsAuthenticated]
 
 class AlbumDetailView(generics.RetrieveAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    permission_classes = [IsAuthenticated]
 
--- spotify/spotify/settings.py
+++ spotify/spotify/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import  timedelta
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'music',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+}
